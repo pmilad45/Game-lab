e257ea87dd7ee4ec339cf003272418505f0fe4f4
DaLotfy31-GUC <you.lotfy@icloud.com>
2026-02-25 01:24:22 +0200

    7atetloko bakeeza ya 7lween

---
+++ Bakeeza.java
@@ -0,0 +1,8 @@
+public class Bakeeza{
+    public static String lotfy(){
+        return "Lotfy is lotfying";
+    }
+    public static void main(String[] args){
+        System.out.print(lotfy());
+    }
+}
